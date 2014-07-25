91ca6ad747f900b725c3b37e1434c40fb9bdee01
jonsjava <jonsjava@gmail.com>
2014-07-25 15:57:04 -0500

    Fixed checkers (newer code)

--- scripts/package_checker.sh
+++ scripts/package_checker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-dpkg -l|grep "ii"|awk '{print $2":::"$3}'
+os=`cat /etc/issue|head -1|awk '{print $1}'`
+if [ "$os" = "CentOS" ]; then
+        rpm -qa --qf '%{NAME}:::%{VERSION}\n'
+elif [ "$os" = "Ubuntu" ]; then
+        dpkg -l|grep "ii"|awk '{print $2":::"$3}'
+fi
--- scripts/patch_checker.sh
+++ scripts/patch_checker.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}'
+os=`cat /etc/issue|head -1|awk '{print $1}'`
+if [ "$os" = "CentOS" ]; then
+        yum -q check-update| while read i
+        do
+                i=$(echo $i) #this strips off yum's irritating use of whitespace
+                if [ "${i}x" != "x" ]
+                then
+                        UVERSION=${i#*\ }
+                        UVERSION=${UVERSION%\ *}
+                        PNAME=${i%%\ *}
+                        PNAME=${PNAME%.*}
+                        echo $(rpm -q "${PNAME}" --qf '%{NAME}:::%{VERSION}:::')${UVERSION}
+                fi
+        done
+elif [ "$os" = "Ubuntu" ]; then
+        apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}'
+fi
