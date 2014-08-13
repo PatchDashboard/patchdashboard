9646aa8f76f9fd4073d0df2a34b2ae34a2dd49f1
jonsjava <jonsjava@gmail.com>
2014-08-13 13:31:13 -0500

    Updated db_config.php to resolve warnings

--- html/lib/db_config.php
+++ html/lib/db_config.php
@@ -1,7 +1,7 @@
 <?php
-define(DB_HOST,"localhost");
-define(DB_USER,"YOUR USER");
-define(DB_PASS,"YOUR PASS");
-define(DB_NAME,"YOUR DATABASE");
-define(BASE_PATH,"/");
-define(YOUR_COMPANY,"Test, INC");
+define('DB_HOST',"localhost");
+define('DB_USER',"YOUR USER");
+define('DB_PASS',"YOUR PASS");
+define('DB_NAME',"YOUR DATABASE");
+define('BASE_PATH',"/");
+define('YOUR_COMPANY',"Test, INC");
