a95b416f70d44a73b72dd650377e38f40c0452f2
metalcated <metalcated@gmail.com>
2014-11-26 13:39:17 -0500

    Improved OS detection

--- scripts/patch_checker.sh
+++ scripts/patch_checker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-os=`cat /etc/issue|head -1|awk '{print $1}'`
+if [[ -f /etc/lsb-release ]]; then
+	export os=$(lsb_release -s -d|head -1|awk {'print $1'})
+elif [[ -f /etc/debian_version ]]; then
+	export os="Debian $(cat /etc/debian_version)|head -1|awk {'print $1'}"
+elif [[ -f /etc/redhat-release ]]; then
+	export os=$(cat /etc/redhat-release|head -1|awk {'print $1'})
+else
+	export os="$(uname -s) $(uname -r)|head -1|awk {'print $1'}"
+fi
 if [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
         yum -q check-update| while read i
         do
@@ -13,6 +21,9 @@ if [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
                         echo $(rpm -q "${PNAME}" --qf '%{NAME}:::%{VERSION}:::')${UVERSION}
                 fi
         done
-elif [ "$os" = "Ubuntu" ]; then
+elif [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ]; then
         apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}'
+elif [ "$os" = "Linux" ]; then
+	echo "unspecified $os not supported"
+	exit 0
 fi
