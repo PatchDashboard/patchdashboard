d68e86e1393becb9bbe29cc759f5850fd17a077e
metalcated <metalcated@gmail.com>
2014-11-26 18:08:00 -0500

    updated script path

--- scripts/queue_patch_check.sh
+++ scripts/queue_patch_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # load DB config
-. /usr/scripts/patch_manager/db.conf
+. /opt/patch_manager/db.conf
 
 # define server from argument
 server="$1"
@@ -12,26 +12,26 @@ sql="DELETE FROM patches where server_name='$server';"
 # define other functions
 supression_list=`mysql -u $DB_USER -p"$DB_PASS" -Nse "SELECT package_name from supressed where server_name in ('$server', 0);" $DB_NAME`
 mysql -u $DB_USER -p"$DB_PASS" -D $DB_NAME -e "$sql" -h $DB_HOST
-script='/usr/scripts/patch_manager/patch_checker.sh'
+script='/opt/patch_manager/patch_checker.sh'
 export server supression_list sql script
 
 function useHost()
 {
-        ssh -i /root/.ssh/id_rsa root@$server 'mkdir -p /usr/scripts/patch_manager/'
-        scp -i /root/.ssh/id_rsa $script root@$server:/usr/scripts/patch_manager/
+        ssh -i /root/.ssh/id_rsa root@$server 'mkdir -p /opt/patch_manager/'
+        scp -i /root/.ssh/id_rsa $script root@$server:/opt/patch_manager/
         ssh -i /root/.ssh/id_rsa root@$server "chmod +x $script"
         data=`ssh -i /root/.ssh/id_rsa root@${server} "bash $script"`
-        /usr/scripts/patch_manager/run_patch_check.php "$server" "$data" "$supression_list"
+        /opt/patch_manager/run_patch_check.php "$server" "$data" "$supression_list"
 }
 
 function useIP()
 {
 	server_ip=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse "SELECT server_ip from servers where server_name='$server';" $DB_NAME`
-	ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /usr/scripts/patch_manager/'
-	scp -i /root/.ssh/id_rsa $script root@$server_ip:/usr/scripts/patch_manager/
+	ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /opt/patch_manager/'
+	scp -i /root/.ssh/id_rsa $script root@$server_ip:/opt/patch_manager/
 	ssh -i /root/.ssh/id_rsa root@$server_ip "chmod +x $script"
 	data=`ssh -i /root/.ssh/id_rsa root@${server_ip} "bash $script"`
-	/usr/scripts/patch_manager/run_patch_check.php "$server" "$data" "$supression_list"
+	/opt/patch_manager/run_patch_check.php "$server" "$data" "$supression_list"
 }
 
 # check provided hostname and if not connect use IP from DB
