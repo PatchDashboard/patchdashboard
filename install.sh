7a54369bd6e44d67593f66168cc334509faacd2c
metalcated <metalcated@gmail.com>
2014-11-26 16:02:47 -0500

    Overhaul of improvements and error handling

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+relative_path="/patch_manager/"
+
 user=`whoami`
 if [ "$user" != "root" ]; then
 	echo "You must be root to install this"
@@ -9,8 +12,20 @@ if [ ! -f /root/.ssh/id_rsa ]; then
 	ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
 	echo "keypair created"
 fi
-os=`cat /etc/issue|head -1|awk '{print $1}'`
-if [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ]; then
+
+if [[ -f /etc/lsb-release ]]; then
+	export os=$(lsb_release -s -d|head -1|awk {'print $1'})
+elif [[ -f /etc/debian_version ]]; then
+	export os="Debian $(cat /etc/debian_version)|head -1|awk {'print $1'}"
+elif [[ -f /etc/redhat-release ]]; then
+	export os=$(cat /etc/redhat-release|head -1|awk {'print $1'})
+else
+	export os="$(uname -s) $(uname -r)|head -1|awk {'print $1'}"
+fi
+
+echo -e "\nRunning install for: \e[32m$os\e[0m\n"
+
+if [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ] || [ "$os" = "Linux" ]; then
 	if [ -d "/var/www/html" ]; then
 		web_dir="/var/www/html/patch_manager/"
 	else
@@ -33,21 +48,94 @@ elif [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
 	web_user="apache"
 	web_service="httpd"
 fi
-relative_path="/patch_manager/"
-echo "Please enter Database Host:"
-read db_host
-echo "Please enter Database User:"
-read db_user
-echo "Please enter Database Password:"
-read -s db_pass
-echo "Please enter the Database Name:"
-read db_name
-mysql -u $db_user -h $db_host -p"$db_pass" -e "create database $db_name;"
-mysql -u $db_user -h $db_host -p"$db_pass" -D $db_name < reports.sql
-echo "Please enter location for web interface [$web_dir]:"
-read new_web_dir
-echo "Please enter the relative path [$relative_path]"
-read new_relative_path
+
+function dbAskHost()
+{
+	echo -e "\e[32m\e[4mDatabase Setup\e[0m\n"
+	unset db_host
+	read -p "Database Host: " db_host
+	while [[ "$db_host" = "" ]]; do
+		echo -e "\e[36mNOTICE\e[0m: Please provide a Database Host, Try again.\n"
+		read -p "Please enter Database Host: " db_host
+	done
+}
+function dbAskUser()
+{
+	unset db_user
+	read -p "Database User: " db_user
+	while [[ "$db_user" = "" ]]; do
+	        echo -e "\e[36mNOTICE\e[0m: Please provide a Database User, Try again.\n"
+	        read -p "Please enter Database User: " db_user
+	done
+}
+function dbAskPass()
+{
+	unset db_pass
+	read -s -p "Database Pass: " db_pass
+	echo
+	while [[ "$db_pass" = "" ]]; do
+        	echo -e "\e[36mNOTICE\e[0m: Please provide a Database Password, Try again.\n"
+        	read -s -p "Please enter Database Pass: " db_pass
+		echo
+	done
+}
+function dbAskName()
+{
+	unset db_name
+	read -p "Database Name: " db_name
+	while [[ "$db_name" = "" ]]; do
+       		echo -e "\e[36mNOTICE\e[0m: Please provide a Database Name, Try again.\n"
+       	 	read -p "Please enter Database Name: " db_name
+	done
+	export db_name
+}
+
+function dbCheck()
+{
+	# check if database exists
+	db_exists=$(mysql --batch -u$db_user -p$db_pass --skip-column-names -e "show databases like '"$db_name"';" | grep "$db_name" > /dev/null; echo "$?")
+	if [ $db_exists -eq 0 ];then
+		dbExists=yes
+	else
+		dbExists=no
+	fi
+}
+
+function dbCreate()
+{
+	mysql -u $db_user -h $db_host -p"$db_pass" -e "create database $db_name;"
+        mysql -u $db_user -h $db_host -p"$db_pass" -D $db_name < reports.sql
+}
+# ask db stuff here
+dbAskHost
+dbAskUser
+dbAskPass
+dbAskName
+dbCheck
+
+while [[ "$dbExists" = "yes" ]]; do
+	echo -e "\n\e[31mERROR\e[0m: \e[36m$db_name\e[0m already exists, enter a different database.\n"
+	dbExists=yes
+	dbAskName
+	dbCheck
+done
+if [[ "$dbExists" = "no" ]]; then
+	dbCreate
+fi
+
+echo -e "\n\e[32m\e[4mWebpage Location Setup\e[0m\n"
+unset new_web_dir
+read -p "Please enter location for web interface [Default: $web_dir]: " new_web_dir
+while [[ "$new_web_dir" = "" ]]; do
+        echo -e "\e[32mNOTICE\e[0m: Default Location Used: $web_dir.\n"
+	new_web_dir=$web_dir
+done
+unset new_relative_path
+read -p "Please enter the relative path [Default: $relative_path]: " new_relative_path
+while [[ "$new_relative_path" = "" ]]; do
+        echo -e "\e[32mNOTICE\e[0m: Default Location Used: $relative_path\n"
+	new_relative_path=$relative_path
+done
 if [ "$new_relative_path" != "$relative_path" ] && [ "$new_relative_path" != "" ]; then
 	relative_path="$new_relative_path"
 fi
@@ -60,16 +148,24 @@ fi
 if [ "${web_dir:LEN}" != "/" ]; then
 	web_dir=$web_dir"/"
 fi
-echo "Please enter the web user [$web_user]:"
-read new_web_user
+unset new_web_user
+read -p "Please enter the web user [Default: $web_user]: " new_web_user
+while [[ "$new_web_user" = "" ]]; do
+        echo -e "\e[32mNOTICE\e[0m: Using Default WebUser $web_user\n"
+        new_web_user=$web_user
+done
 if [ "$new_web_user" != "$web_user" ] && [ "$new_web_user" != "" ]; then
 	web_user="$new_web_user"
 fi
-echo "Please enter the name you want this copyrighted to ['YOUR COMPANY']:"
-read your_company
-if [ "$your_company" = "" ]; then
+unset your_company
+read -p "Please enter the name you want this copyrighted to ['YOUR COMPANY']: " your_company
+while [ "$your_company" = "" ]; do
 	your_company='YOUR COMPANY'
-fi
+	echo -e "\e[32mNOTICE\e[0m: Using Default Company $your_company\n"
+done
+
+echo -e "\e[32m\e[4mInstalling Apache related configurations\e[0m\n"
+
 rewrite_config="RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
@@ -107,8 +203,9 @@ bash_config="DB_HOST='$db_host'
 DB_USER='$db_user'
 DB_PASS='$db_pass'
 DB_NAME='$db_name'"
-mkdir -p /root/scripts/
-cp scripts/* /root/scripts/ -R
+target_web_dir=$(echo $new_web_dir|sed 's=/[^/]*$==;s/\.$//')
+mkdir -p /usr/scripts/patch_manager/
+cp scripts/* /usr/scripts/patch_manager/ -R
 echo "$rewrite_config" > html/.htaccess
 echo "$php_config" > html/lib/db_config.php
 mkdir -p $web_dir
@@ -117,16 +214,16 @@ find $web_dir -type d -print0|xargs -0 chmod 755
 find $web_dir -type f -print0|xargs -0 chmod 644
 echo "$rewrite_config" > ${web_dir}.htaccess
 chown $web_user:$web_user $web_dir -R
-echo "$php_config" > /root/scripts/db_config.php
-echo "$bash_config" > /root/scripts/db.conf
+echo "$php_config" > /usr/scripts/patch_manager/db_config.php
+echo "$bash_config" > /usr/scripts/patch_manager/db.conf
 service $web_service restart
 rewrite_check=`curl -s localhost/rewrite_check|grep 404|wc -l`
 if [ "$rewrite_check" = "1" ]; then
-	echo "Apache Mod_rewrite or .htaccess is not working.  Please ensure you have mod_rewrite installed and enabled.  If it is, please make sure you change 'AllowOverride None' to 'AllowOverride All'"
-	echo "If you don't, this site won't work.  You've been warned."
+	echo -e "\n\e[31mERROR\e[0m: Apache Mod_rewrite or .htaccess is not working.  Please ensure you have mod_rewrite installed and enabled.  If it is, please make sure you change 'AllowOverride None' to 'AllowOverride All'"
+	echo -e "\e[31mERROR\e[0m: If you don't, this site won't work.  You've been warned."
 fi
-echo "Install is now complete. You can now go to http://localhost$relative_path and begin working with this tool.  To add servers, use the following command:
-	/root/scripts/add_server.sh 'server_name' 'IP_ADDRESS'
+echo -e "\n\e[32mNOTICE\e[0m: Install is now complete. You can now go to http://localhost$relative_path and begin working with this tool.  To add servers, use the following command:
+	/usr/scripts/patch_manager/add_server.sh -s server_name -ip IP_ADDRESS
 	It will ask you some questions regarding the user, password, and some other things.  Just follow the prompts.
 	
 	To add a server without the script granting super user, you will need to do the following:
