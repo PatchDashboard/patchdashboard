0ed8e0da00786d7ac8249a1ab5e1ccfc087cc53a
metalcated <metalcated@gmail.com>
2014-11-26 18:06:22 -0500

    updated script path

--- scripts/queue_package_check.sh
+++ scripts/queue_package_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # load DB config
-. /usr/scripts/patch_manager/db.conf
+. /opt/patch_manager/db.conf
 
 # define server from argument
 server="$1"
@@ -11,26 +11,26 @@ sql="DELETE FROM patch_allpackages where server_name='$server';"
 
 # define other functions
 mysql -u $DB_USER -p"$DB_PASS" -D $DB_NAME -e "$sql" -h $DB_HOST
-script='/usr/scripts/patch_manager/package_checker.sh'
+script='/opt/patch_manager/package_checker.sh'
 export server sql script
 
 function useHost()
 {
-        ssh -i /root/.ssh/id_rsa root@$server 'mkdir -p /usr/scripts/patch_manager/'
-        scp -i /root/.ssh/id_rsa $script root@$server:/usr/scripts/patch_manager/
+        ssh -i /root/.ssh/id_rsa root@$server 'mkdir -p /opt/patch_manager/'
+        scp -i /root/.ssh/id_rsa $script root@$server:/opt/patch_manager/
         ssh -i /root/.ssh/id_rsa root@$server "chmod +x $script"
         data=`ssh -i /root/.ssh/id_rsa root@${server} "bash $script"`
-        /usr/scripts/patch_manager/run_get_package_list.php "$server" "$data"
+        /opt/patch_manager/run_get_package_list.php "$server" "$data"
 }
 
 function useIP()
 {
         server_ip=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse "SELECT server_ip from servers where server_name='$server';" $DB_NAME`
-        ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /usr/scripts/patch_manager/'
-        scp -i /root/.ssh/id_rsa $script root@$server_ip:/usr/scripts/patch_manager/
+        ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /opt/patch_manager/'
+        scp -i /root/.ssh/id_rsa $script root@$server_ip:/opt/patch_manager/
         ssh -i /root/.ssh/id_rsa root@$server_ip "chmod +x $script"
         data=`ssh -i /root/.ssh/id_rsa root@${server_ip} "bash $script"`
-        /usr/scripts/patch_manager/run_get_package_list.php "$server" "$data"
+        /opt/patch_manager/run_get_package_list.php "$server" "$data"
 }
 
 # check provided hostname and if not connect use IP from DB
