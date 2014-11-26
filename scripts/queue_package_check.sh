34e342cb4c30fc3a276bda913a3af0752d870a82
metalcated <metalcated@gmail.com>
2014-11-26 14:07:32 -0500

    Improved functionality and abilty to use hosts instead of IP

--- scripts/queue_package_check.sh
+++ scripts/queue_package_check.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
-. /root/scripts/db.conf
+
+# load DB config
+. /usr/scripts/patch_manager/db.conf
+
+# define server from argument
 server="$1"
-i=$1
+
+# connect to DB and clean up packages
 sql="DELETE FROM patch_allpackages where server_name='$server';"
+
+# define other functions
 mysql -u $DB_USER -p"$DB_PASS" -D $DB_NAME -e "$sql" -h $DB_HOST
-script='/root/scripts/package_checker.sh'
-server_ip=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse "SELECT server_ip from servers where server_name='$server';" $DB_NAME`
-ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /root/scripts/'
-scp -i /root/.ssh/id_rsa $script root@$server_ip:~/scripts/
-ssh -i /root/.ssh/id_rsa root@$server_ip "chmod +x $script"
-data=`ssh -i /root/.ssh/id_rsa root@${server_ip} "bash $script"`
-/root/scripts/run_get_package_list.php "$server" "$data"
+script='/usr/scripts/patch_manager/package_checker.sh'
+export server sql script
+
+function useHost()
+{
+        ssh -i /root/.ssh/id_rsa root@$server 'mkdir -p /usr/scripts/patch_manager/'
+        scp -i /root/.ssh/id_rsa $script root@$server:/usr/scripts/patch_manager/
+        ssh -i /root/.ssh/id_rsa root@$server "chmod +x $script"
+        data=`ssh -i /root/.ssh/id_rsa root@${server} "bash $script"`
+        /usr/scripts/patch_manager/run_get_package_list.php "$server" "$data"
+}
+
+function useIP()
+{
+        server_ip=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse "SELECT server_ip from servers where server_name='$server';" $DB_NAME`
+        ssh -i /root/.ssh/id_rsa root@$server_ip 'mkdir -p /usr/scripts/patch_manager/'
+        scp -i /root/.ssh/id_rsa $script root@$server_ip:/usr/scripts/patch_manager/
+        ssh -i /root/.ssh/id_rsa root@$server_ip "chmod +x $script"
+        data=`ssh -i /root/.ssh/id_rsa root@${server_ip} "bash $script"`
+        /usr/scripts/patch_manager/run_get_package_list.php "$server" "$data"
+}
+
+# check provided hostname and if not connect use IP from DB
+ping -c 1 $server > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+        useHost
+else
+        useIP
+fi
