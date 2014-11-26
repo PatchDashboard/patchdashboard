5b56491aa3f30e40587b224a42ab855f3d4e49db
metalcated <metalcated@gmail.com>
2014-11-26 18:10:54 -0500

    updated script path

--- scripts/start_patch_check.sh
+++ scripts/start_patch_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-. /root/scripts/db.conf
+. /opt/patch_manager/db.conf
 server_list=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse 'SELECT server_name from servers' $DB_NAME`
-for i in `echo $server_list`; do
-        /root/scripts/queue_patch_check.sh "$i" &
+for server in `echo $server_list`; do
+        /opt/patch_manager/queue_patch_check.sh "$server" &
 done
