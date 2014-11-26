a3666e1f656f8045fb516e7cd89c92da465306de
metalcated <metalcated@gmail.com>
2014-11-26 18:11:10 -0500

    Update start_get_package_list.sh

--- scripts/start_get_package_list.sh
+++ scripts/start_get_package_list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . /opt/patch_manager/db.conf
 server_list=`mysql -h $DB_HOST -u $DB_USER -p"$DB_PASS" -Nse 'SELECT server_name from servers' $DB_NAME`
-for i in `echo $server_list`; do
-	/opt/patch_manager/queue_package_check.sh "$i" &
+for server in `echo $server_list`; do
+	/opt/patch_manager/queue_package_check.sh "$server" &
 done
