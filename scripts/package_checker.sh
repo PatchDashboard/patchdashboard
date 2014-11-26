00934125d48577066b54eba34760b19f9c07f3f1
metalcated <metalcated@gmail.com>
2014-11-26 13:37:45 -0500

    Improved OS detection

--- scripts/package_checker.sh
+++ scripts/package_checker.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-os=`cat /etc/issue|head -1|awk '{print $1}'`
-if [ "$os" = "CentOS" ]; then
+if [[ -f /etc/lsb-release ]]; then
+        export os=$(lsb_release -s -d|head -1|awk {'print $1'})
+elif [[ -f /etc/debian_version ]]; then
+        export os="Debian $(cat /etc/debian_version)|head -1|awk {'print $1'}"
+elif [[ -f /etc/redhat-release ]]; then
+        export os=$(cat /etc/redhat-release|head -1|awk {'print $1'})
+else
+        export os="$(uname -s) $(uname -r)|head -1|awk {'print $1'}"
+fi
+if [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
         rpm -qa --qf '%{NAME}:::%{VERSION}\n'
-elif [ "$os" = "Ubuntu" ]; then
-        dpkg -l|grep "ii"|awk '{print $2":::"$3}'
+elif [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ]; then
+	dpkg -l|grep "ii"|awk '{print $2":::"$3}'
+elif [ "$os" = "Linux" ]; then
+	echo "unspecified $os not supported"
+	exit 0
 fi
