3d2db18d146b39a999d2de40761ccd13a43de805
jonsjava <jonsjava@gmail.com>
2014-12-15 10:06:03 -0600

    First push with client-side push of patch list to server via HTTP(S)

--- html/client/send_patches.php
+++ html/client/send_patches.php
@@ -1,8 +1,40 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+include '../lib/db_config.php';
+$client_key = filter_input(INPUT_SERVER, 'HTTP_X_CLIENT_KEY');
+$client_check_sql = "SELECT `server_name` FROM `servers` WHERE `client_key` = '$client_key' AND `trusted`=1 LIMIT 1;";
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+mysql_select_db(DB_NAME, $link);
+$client_check_res = mysql_query($client_check_sql);
+if (mysql_num_rows($client_check_res) == 1) {
+    $row = mysql_fetch_array($client_check_res);
+    $server_name = $row['server_name'];
+    $package_array = explode("\n", $data);
+    $supression_array = explode(" ", $supression_list);
+    foreach ($package_array as $val) {
+        $tmp_array = explode(":::", $val);
+        $package_name = $tmp_array[0];
+        $package_from = $tmp_array[1];
+        $package_to = $tmp_array[2];
+        $bug_curl = shell_exec("bash -c \"curl -s http://www.ubuntuupdates.org/bugs?package_name=$package_name|grep '<td>' 2>/dev/null|head -1\"");
+        $url = str_replace("<td><a href='", "", $bug_curl);
+        $url_array = explode("'", $url);
+        $the_url = $url_array[0];
+        $urgency_curl = shell_exec("bash -c \"curl http://www.ubuntuupdates.org/package/core/precise/main/updates/$package_name|grep '$package_to'|grep 'urgency='\"");
+        if (stristr($urgency_curl, "emergency")) {
+            $urgency = "emergency";
+        } elseif (stristr($urgency_curl, "high")) {
+            $urgency = "high";
+        } elseif (stristr($urgency_curl, "medium")) {
+            $urgency = "medium";
+        } elseif (stristr($urgency_curl, "low")) {
+            $urgency = "low";
+        } else {
+            $urgency = "unknown";
+        }
+        if (!in_array($package_name, $supression_array)) {
+            $sql = "INSERT INTO patches(server_name,package_name,current,new,urgency,bug_url) VALUES('$server_name','$package_name','$package_from','$package_to','$urgency','$the_url');";
+            mysql_query($sql);
+        }
+    }
+}
+mysql_close();
--- scripts/patch_checker.sh
+++ scripts/patch_checker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# generated installation key from install
+# generated installation key and server URI from install
 auth_key="000DEFAULT000"
-
+server_uri="__SERVER_URI_SET_ME__"
+submit_patch_uri="${server_uri}client/send_patches.php"
 # remove old file is blank
 if [[ -s /opt/patch_manager/.patchrc ]]; then
         rm -rf /opt/patch_manager/.patchrc
@@ -20,7 +21,7 @@ if [[ ! -f "/opt/patch_manager/.pachrc" ]]; then
 fi
 # load the file
 . /opt/patch_manager/.patchrc
-
+rm -rf /tmp/$client_key
 if [[ -f /etc/lsb-release ]]; then
 	export os=$(lsb_release -s -d|head -1|awk {'print $1'})
 elif [[ -f /etc/debian_version ]]; then
@@ -31,6 +32,7 @@ else
 	export os="$(uname -s) $(uname -r)|head -1|awk {'print $1'}"
 fi
 if [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
+		need_patched="true"
         yum -q check-update| while read i
         do
                 i=$(echo $i) #this strips off yum's irritating use of whitespace
@@ -41,11 +43,21 @@ if [ "$os" = "CentOS" ] || [ "$os" = "Fedora" ] || [ "$os" = "Red" ]; then
                         PNAME=${i%%\ *}
                         PNAME=${PNAME%.*}
                         echo $(rpm -q "${PNAME}" --qf '%{NAME}:::%{VERSION}:::')${UVERSION}
+						patches_to_install=$(echo $(rpm -q "${PNAME}" --qf '%{NAME}:::%{VERSION}:::')${UVERSION})
+						echo "$patches_to_install" >> /tmp/$client_key
                 fi
         done
 elif [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ]; then
+		need_patched="true"
         apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}'
+		patches_to_install=$(apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}')
+		echo "$patches_to_install\n" >> /tmp/$client_key
 elif [ "$os" = "Linux" ]; then
 	echo "unspecified $os not supported"
 	exit 0
+fi
+if [ "$need_patched" = "true" ]; then
+	patch_list=$(cat /tmp/$client_key)
+	curl -H "X-CLIENT-KEY: $client_key" $submit_patch_uri -d "$patch_list" > /dev/null 2>&1
+	rm -rf /tmp/$client_key
 fi
