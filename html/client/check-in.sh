07c59190c3f478c5b1096c91ba3a517af886e3c9
metalcated <mike.gomon@gmail.com>
2014-12-31 14:00:23 -0500

    Reworked Logic for client_key

--- html/client/check-in.sh
+++ html/client/check-in.sh
@@ -2,6 +2,7 @@
 auth_key="__SERVER_AUTHKEY_SET_ME__"
 server_uri="__SERVER_URI_SET_ME__"
 check_in="${server_uri}client/check-in.php"
+client_host=$(hostname -f)
 
 if [[ -f /etc/lsb-release ]]; then
         export client_os=$(lsb_release -s -d|head -1|awk {'print $1'})
@@ -18,16 +19,17 @@ else
 fi
 
 if [ ! -f /opt/patch_manager/.patchrc ]; then
-        host=$(hostname -f)
         random_bits=$(< /dev/urandom tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | head -c${1:-32})
-        client_key=$(echo "${host}${random_bits}"|sha256sum|cut -d ' ' -f 1)
+        client_key=$(echo "${client_host}${random_bits}"|sha256sum|cut -d ' ' -f 1)
         if [[ ! -d /opt/patch_manager ]]; then
                 mkdir -p /opt/patch_manager
         fi
         echo "client_key=\"$client_key\"" > /opt/patch_manager/.patchrc
+else
+        client_key=$(grep client_key /opt/patch_manager/.patchrc|awk -F\" {'print $2'})
 fi
 . /opt/patch_manager/.patchrc
-curl -k -s -H "X-CLIENT-KEY: $client_key" -H "X-CLIENT-HOST: $host" -H "X-CLIENT-OS: $client_os" -H "X-CLIENT-OSVER: $client_os_ver" $check_in > /tmp/check-in_$client_key
+curl -k -s -H "X-CLIENT-KEY: $client_key" -H "X-CLIENT-HOST: $client_host" -H "X-CLIENT-OS: $client_os" -H "X-CLIENT-OSVER: $client_os_ver" $check_in > /tmp/check-in_$client_key
 cmds_line_count=$(cat /tmp/check-in_$client_key|wc -l)
 if [ "$cmds_line_count" -gt "1" ]; then
         . /tmp/check-in_$client_key
