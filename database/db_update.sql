9faeb28c30868c1fc161b0cbfbc247a679fc1f22
Samuel <faust64@gmail.com>
2016-07-11 13:17:05 +0200

    Add Xenial (2/4)

--- database/db_update.sql
+++ database/db_update.sql
@@ -117,6 +117,8 @@ INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('21'
 INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('22','6',6,'2020-11-30');
 INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('23','7',6,'2024-06-30');
 INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('24','8',2,'2020-05-01');
+INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('25','16.04_Desktop',1,'2021-04-30');
+INSERT IGNORE INTO distro_version(id,version_num,distro_id,eol_date) VALUES('26','16.04_Server',1,'2021-04-30');
 DROP table distro_version_old;
 
 DROP TABLE IF EXISTS patches_old;
